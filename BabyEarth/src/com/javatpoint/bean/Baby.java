4f1e2416200346d0eaef9dc5fc96585bfb36255f
ilaydabykakova <ilaydabuyukakova27@gmail.com>
2020-11-06 23:36:13 +0300

    upload

---
+++ BabyEarth/src/com/javatpoint/bean/Baby.java
@@ -0,0 +1,37 @@
+package com.javatpoint.bean;
+
+public class Baby {
+int id;
+String name,meaning,gender,religion;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getMeaning() {
+	return meaning;
+}
+public void setMeaning(String meaning) {
+	this.meaning = meaning;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getReligion() {
+	return religion;
+}
+public void setReligion(String religion) {
+	this.religion = religion;
+}
+
+}
---
+++ BabyEarth/src/com/javatpoint/servlets/ViewByStart.java
@@ -0,0 +1,51 @@
+package com.javatpoint.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javatpoint.bean.Baby;
+import com.javatpoint.dao.BabyDao;
+import javax.servlet.http.*;
+@WebServlet("/ViewByStart")
+public class ViewByStart extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String start=request.getParameter("start");
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>View Baby by "+start.toUpperCase()+"</title>");
+		out.println("<link rel='stylesheet' href='resources/bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navigation.html").include(request, response);
+		out.print(" <a href='ViewBabyNameByReligion?religion=Hindu' class='btn btn-primary' role='button'>Hindu</a> ");
+		out.print(" <a href='ViewBabyNameByReligion?religion=Muslim' class='btn btn-primary' role='button'>Muslim</a> ");
+		out.print(" <a href='ViewBabyNameByReligion?religion=Jewish' class='btn btn-primary' role='button'>Jewish</a> ");
+		out.print(" <a href='ViewBabyNameByReligion?religion=Christian' class='btn btn-primary' role='button'>Christian</a> ");
+		out.print(" <a href='ViewBabyNameByReligion?religion=Other' class='btn btn-primary' role='button'>Other</a> ");
+		request.getRequestDispatcher("atoz.html").include(request, response);
+		
+		out.println("<h1>View Baby Names by "+start.toUpperCase()+"</h1>");
+		List<Baby> list=BabyDao.getRecordsByStart(start);
+		
+		out.print("<table class='table table-bordered table-striped'>");
+		out.println("<tr><th>Id</th><th>Name</th><th>Meaning</th><th>Gender</th><th>Religion</th></tr>");
+		for(Baby b:list){
+			out.println("<tr><td>"+b.getId()+"</td><td>"+b.getName()+"</td><td>"+b.getMeaning()+"</td><td>"+b.getGender()+"</td><td>"+b.getReligion()+"</td></tr>");
+		}
+		out.println("</table>");
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
+	}
+
+}
